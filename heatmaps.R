2a4c0f47bcb8d29ff8310ac6721c64b70b39432b
Alexander-Palmer <50350618+Alexander-Palmer@users.noreply.github.com>
2019-05-13 12:12:59 +1000

    Update heatmaps.R

--- heatmaps.R
+++ heatmaps.R
@@ -27,11 +27,13 @@ rownames(o) <- (n[,1])
 o <- as.matrix(o)
 
 #Heatmap
-superheat(o, left.label.size = 0.2, bottom.label.size = 0.05, scale = FALSE, 
-          row.dendrogram = TRUE, title = "ALG1 miRNA logFC Tissue loading comparison", 
-          row.title = "miRNA", column.title = "Tissue", left.label.text.size = 2, 
-          grid.hline.col = "white", grid.vline.col = "white", grid.hline.size = 500, 
-          grid.vline.size = 1, legend.height = 0.05, bottom.label.text.size = 3)
+superheat(o, left.label.size = 0.2, bottom.label.size = 0.05, scale = FALSE,
+          row.dendrogram = TRUE, title = "ALG1 miRNA logFC Tissue loading comparison",
+          row.title = "miRNA", column.title = "Tissue", left.label.text.size = 2,
+          grid.hline.col = "white", grid.vline.col = "white", grid.hline.size = 500,
+          grid.vline.size = 2., legend.height = 0.06, bottom.label.text.size = 3,
+          heat.pal.values = c(0, 0.5, 1), legend.num.ticks = 10, legend.width = 1.8,
+          legend.text.size = 11)
           
 #########################
 #Output
